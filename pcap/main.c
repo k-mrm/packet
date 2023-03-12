1d567d2227cb56fe9282394b0002892c65ff339f
Keisuke Iida <cmpl.error@gmail.com>
2023-03-13 02:24:49 +0900

    pcap: ipv6

--- pcap/main.c
+++ pcap/main.c
@@ -12,6 +12,7 @@
 #include <net/if.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <netinet/ip6.h>
 #include <netinet/if_ether.h>
 #include <linux/if_packet.h>
 
@@ -24,10 +25,19 @@ hwaddrfmt(unsigned char *mac, char *str) {
 }
 
 static void
-ipaddrfmt(uint32_t ipaddr, char *str) {
+ipv4addrfmt(uint32_t ipaddr, char *str) {
   uint8_t *ip = (uint8_t *)&ipaddr;
 
   snprintf(str, 32, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+}
+
+static void
+ipv6addrfmt(struct in6_addr *ipaddr, char *str) {
+  uint16_t *addr = (uint16_t *)ipaddr->s6_addr;
+
+  snprintf(str, 64, "%x:%x:%x:%x:%x:%x:%x:%x",
+           ntohs(addr[0]), ntohs(addr[1]), ntohs(addr[2]), ntohs(addr[3]),
+           ntohs(addr[4]), ntohs(addr[5]), ntohs(addr[6]), ntohs(addr[7]));
 }
 
 static const char *
@@ -43,20 +53,29 @@ ip_protocol_fmt(uint8_t proto) {
 }
 
 static void
-ipv4_packet_dump(unsigned char *buf, size_t size) {
+ipv4_packet_dump(unsigned char *buf) {
   struct iphdr *iphdr = (struct iphdr *)buf;
   char dst[32] = {0};
   char src[32] = {0};
 
-  ipaddrfmt(iphdr->daddr, dst);
-  ipaddrfmt(iphdr->saddr, src);
+  ipv4addrfmt(iphdr->daddr, dst);
+  ipv4addrfmt(iphdr->saddr, src);
 
-  printf("ipv4 %s ---> %s %s\n", src, dst, ip_protocol_fmt(iphdr->protocol));
+  printf("ipv4 %s ---> %s %s", src, dst, ip_protocol_fmt(iphdr->protocol));
 }
 
 static void
-ipv6_packet_dump(unsigned char *buf, size_t size) {
-  ;
+ipv6_packet_dump(unsigned char *buf) {
+  struct ip6_hdr *ip6 = (struct ip6_hdr *)buf;
+  struct in6_addr *dst = &ip6->ip6_dst;
+  struct in6_addr *src = &ip6->ip6_src;
+  char d_str[64];
+  char s_str[64];
+
+  ipv6addrfmt(dst, d_str);
+  ipv6addrfmt(src, s_str);
+
+  printf("ipv6 %s ---> %s", s_str, d_str);
 }
 
 static void
@@ -68,12 +87,14 @@ packetdump(unsigned char *buf, size_t size) {
 
   switch(type) {
     case 0x0800:    // ipv4
-      ipv4_packet_dump(buf, size);
+      ipv4_packet_dump(buf);
       break;
     case 0x86dd:    // ipv6
-      ipv6_packet_dump(buf, size);
+      ipv6_packet_dump(buf);
       break;
   }
+
+  printf(" size %ld\n", size);
 }
 
 static int
